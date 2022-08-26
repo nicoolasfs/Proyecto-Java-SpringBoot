0647935680759e6da1099cf46c2bf9560d27abfa
Nicolas <nfonseca1999@gmail.com>
2022-08-26 18:33:52 -0500

    Iniciamos el proyecto SpringBoot

---
+++ src/main/java/net/proyectojava/backendjava/BackendjavaApplication.java
@@ -0,0 +1,14 @@
+package net.proyectojava.backendjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendjavaApplication.class, args);
+		System.out.println("Working...");
+	}
+
+}
